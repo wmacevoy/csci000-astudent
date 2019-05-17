c464dc44fdd34bddd0e21cb532cd8a730fd8d997
Warren MacEvou <wmacevoy@gmail.com>
2019-05-16 22:59:00 -0600

    all tests pass

--- hello/src/test_hello.cpp
+++ hello/src/test_hello.cpp
@@ -5,12 +5,32 @@ using namespace std;
 using namespace hello;
 
 // https://github.com/google/googletest/blob/master/googletest/docs/primer.md
+TEST(Greeter, ConstructorDefault) {
+  Greeter greeter;
+  ASSERT_EQ(greeter.name,"World");
+}
+
+TEST(Greeter, ConstructorAlice) {
+  string name = "Alice";
+  Greeter greeter(name);
+  ASSERT_EQ(greeter.name,name);
+}
 
 TEST(Greeter, GreetWorld) {
   ostringstream expect;
   expect << "Hello, World!" << endl;
   ostringstream result;
   Greeter greeter;
+  greeter.greet(result);
+  ASSERT_EQ(expect.str(),result.str());
+}
+
+TEST(Greeter, GreetAlice) {
+  string name="Alice";
+  ostringstream expect;
+  expect << "Hello, " << name << "!" << endl;
+  ostringstream result;
+  Greeter greeter(name);
   greeter.greet(result);
   ASSERT_EQ(expect.str(),result.str());
 }
